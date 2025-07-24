4af01c8f6116bc166377694db4f7e23308b44450
DanielButnaru <89221551+DanielButnaru@users.noreply.github.com>
2025-07-24 13:09:53 +0300

    fix

--- src/pages/OnBoarding.tsx
+++ src/pages/OnBoarding.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { ShoppingBag, Zap, Shield, Users } from "lucide-react";
 import { Link } from "react-router-dom";
 
